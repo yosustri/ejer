aa888948e5d4ce3c584ae896c88c5a3a4bb435d4
yosustri <yosustri@aol.com>
2012-10-08 23:13:59 +0300

    Update src/ejer/ListaCandidatos.java

--- src/ejer/ListaCandidatos.java
+++ src/ejer/ListaCandidatos.java
@@ -5,47 +5,22 @@ import java.util.Iterator;
 
 public class ListaCandidatos {
 
-	public static void main(String[] args) 
-	{
-	 Candidato c1,c2,c3,c4,c5;
-	 Partido p1;
-	// Distrito d1;
-	//ListaDistritos listd1;
-	Municipio m1;
-
-		m1=new Municipio("Barakaldo");
-		p1=new Partido("PNV");
-		//d1=new Distrito("0001",m1);
-		c1=new Candidato("Pepe", "Alvarez",p1);
-		c2=new Candidato("Ana", "Arenaza", p1);
-		c3=new Candidato("Maria", "Bonilla", p1);
-		c4=new Candidato("Juana", "Zapata", p1);
-		c5=new Candidato("Pedro", "Alvarez", p1);
-		miLista.agregarCandidato(c5);
-		miLista.agregarCandidato(c1);
-		miLista.agregarCandidato(c4);
-		miLista.agregarCandidato(c2);
-		miLista.agregarCandidato(c3);
-		miLista.eliminarCandidato(c1);
-		/*miLista.eliminarCandidato(c2);
-		miLista.eliminarCandidato(c3);
-		miLista.eliminarCandidato(c4);*/
-		miLista.imprimirLista();
-	}  
-
 private ArrayList<Candidato> candidatos;
 private static ListaCandidatos miLista=new ListaCandidatos();
 
 private ListaCandidatos(){
 this.candidatos=new ArrayList<Candidato>();
 }
+
 public static ListaCandidatos getListaCandidatos()
 {
 return miLista;
 }
+
 private ArrayList<Candidato> getLista() {
 return candidatos;
 }
+
 public int getTamano()
 {
 return ListaCandidatos.getListaCandidatos().getLista().size();
@@ -56,40 +31,36 @@ return ListaCandidatos.getListaCandidatos().getLista().iterator();
 }
 public int  buscarCandidato(Candidato pCandidato)
 {	
-Candidato unCandidato;
-int min,mitad,max;
-boolean enc=false;
-min=0;
-max=this.getTamano()-1;
-mitad=(int)(max+min)/2;   	//Redondea a lo bajo
-unCandidato=this.getLista().get(mitad);
-if(this.getTamano()==1&&this.getLista().get(0).compareTo(unCandidato, pCandidato)>0)
-{
-	return 0;
-}
-while (!enc)
-{
-	if(unCandidato==pCandidato)
-	{
-		enc=true;
-		return mitad;
-	}
-		if(unCandidato.compareTo(unCandidato, pCandidato)<0)
-	{
-		min=mitad+1;
-	}
-	else
-	{
-		max=mitad-1;
-	}
-	mitad=(int)(max+min)/2;
+	Candidato unCandidato;
+	int min,mitad,max;
+	min=0;
+	max=this.getTamano()-1;
+	mitad=0;
+	mitad=(int)(max+min)/2;   	//Redondea a lo bajo
 	unCandidato=this.getLista().get(mitad);
-	if(min>max)
+	if(this.getTamano()==1&&this.getLista().get(0).compareTo(pCandidato)>0)
 	{
-		return mitad+1;
+		return 0;
 	}
-}
-return -1;
+	while (min<=max)
+	{
+		
+		if(unCandidato.compareTo(pCandidato)<0)
+		{
+			min=mitad+1;
+		}
+		else if (unCandidato.compareTo(pCandidato)>0) 
+		{
+			max=mitad-1;	
+		}
+		else
+		{
+			return mitad;
+		}
+		mitad=(int)(max+min)/2;
+		unCandidato=this.getLista().get(mitad);
+	}
+	return mitad+1;
 }
 
     
@@ -102,25 +73,24 @@ public void agregarCandidato(Candidato pCandidato){//Ordenadamente
     else
     {
 	int indice =this.buscarCandidato(pCandidato);
+	//System.out.println(indice);
 	ListaCandidatos.getListaCandidatos().getLista().add(indice,pCandidato);
     }
     }
     
 public void eliminarCandidato(Candidato pCandidato){
-	if(this.getTamano()==0){
+	int indice=ListaCandidatos.getListaCandidatos().buscarCandidato(pCandidato);
+	if(ListaCandidatos.getListaCandidatos().getTamano()==0){
 		System.out.println("Lista vacia imposible borrar");
 		}
-	else{
-		int indice=this.buscarCandidato(pCandidato);
-		if(this.getLista().get(indice)!=pCandidato)
-			{
-			System.out.println("No existe ese candidato");
+	else if (ListaCandidatos.getListaCandidatos().getLista().get(indice).compareTo(pCandidato)==0) 
+		{	
+		ListaCandidatos.getListaCandidatos().getLista().remove(indice);	
 			}
 		else
 			{
-			ListaCandidatos.getListaCandidatos().getLista().remove(indice);	
+			System.out.println("No existe ese candidato");
 			}
-		}
     }
 
 public void imprimirLista()
@@ -132,5 +102,14 @@ public void imprimirLista()
 		unCandidato=it.next();
 		System.out.println(unCandidato.getApellido()+" "+unCandidato.getNombre());
 	}
+}
+public boolean estaEnLista(Candidato pCandidato)
+{
+	int indice =this.buscarCandidato(pCandidato);
+	if(ListaCandidatos.getListaCandidatos().getLista().get(indice).compareTo(pCandidato)==0)
+	{
+		return true;
+	}
+	return false;
 }
 }
